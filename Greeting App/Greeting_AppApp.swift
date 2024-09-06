2039fd0b46bd5c5f4ec0452513f74f9fe0d18e61
tyler0611 <berlity0611@gmail.com>
2024-09-06 09:48:42 -0500

    Initial Commit

---
+++ Greeting App/Greeting_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Greeting_AppApp.swift
+//  Greeting App
+//
+//  Created by Tyler Berlin on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Greeting_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
