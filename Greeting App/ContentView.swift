370d321535bef151d67f1070c35af6900892788d
tyler0611 <berlity0611@gmail.com>
2024-09-17 10:58:29 -0500

    Greeting App

--- Greeting App/ContentView.swift
+++ Greeting App/ContentView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var name = ""
+    @State private var greeting = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Greeting App")
+                .font(.title).bold()
+            TextField("Enter Name", text: $name)
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(.center)
+                .frame(width: 200, height: 30)
+                .font(.body)
+                .padding()
+            HStack{
+                Button("Hello"){
+                    greeting = "Hello, \(name)!"
+                }
+                .font(.system(size: 24))
+                Button("Goodbye"){
+                    greeting = "Goodbye, \(name)!"
+                }
+                .font(.system(size: 24))
+                Button("Aloha"){
+                    greeting = "Aloha, \(name)!"
+                }
+                .font(.system(size: 24))
+            }
+            .padding()
+            Text(greeting)
+                .font(.title).bold()
         }
         .padding()
+        Spacer()
     }
 }
 
